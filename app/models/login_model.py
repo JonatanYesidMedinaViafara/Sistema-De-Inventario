f20bd75b3a293eb0f386c986d58d305e7052e103
Jonatan Medina <Jonatan.Medina@correounivalle.edu.co>
2024-10-30 14:50:06 -0500

    mejorando la conexion a login desde una manera indepediente

--- app/models/login_model.py
+++ app/models/login_model.py
@@ -1,16 +1,19 @@
 import psycopg2
 from psycopg2 import Error
+from config import Config  # Se importa la configuración desde config.py
 
 # Función para conectar a la base de datos
 def connect_to_db():
     try:
         connection = psycopg2.connect(
-            user="postgres",
-            password="Americ@1927",
-            host="127.0.0.1",
-            port="5432",
-            database="sistema_inventario_BD"
+            user=Config.DB_USER,
+            password=Config.DB_PASSWORD,
+            host=Config.DB_HOST,
+            port=Config.DB_PORT,
+            database=Config.DB_NAME
         )
+        print("Conexión exitosa a la base de datos.")
         return connection
     except Error as e:
         print("Error al conectar a la base de datos:", e)
+        return None
